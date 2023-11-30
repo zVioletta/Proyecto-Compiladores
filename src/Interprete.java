273f76590ad0c261aebc5a7d62958c8016af419b
zVioletta <92064685+zVioletta@users.noreply.github.com>
2023-11-30 15:07:41 -0600

    Si

--- src/Interprete.java
+++ src/Interprete.java
@@ -11,13 +11,8 @@ public class Interprete {
     static boolean existenErrores = false;
 
     public static void main(String[] args) throws IOException {
-
-        // Verifica la cantidad de argumentos en la línea de comandos
-
         if(args.length > 1) {
             System.out.println("Uso correcto: interprete [archivo.txt]");
-
-            // Convención defininida en el archivo "system.h" de UNIX
             System.exit(64);
         } else if(args.length == 1){
             ejecutarArchivo(args[0]);
@@ -27,13 +22,8 @@ public class Interprete {
     }
 
     private static void ejecutarArchivo(String path) throws IOException {
-
-      // Lee el contenido del archivo y lo ejecuta
-
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         ejecutar(new String(bytes, Charset.defaultCharset()));
-
-        // Se indica que existe un error
         if(existenErrores) System.exit(65);
     }
 
@@ -44,9 +34,9 @@ public class Interprete {
         for(;;){
             System.out.print(">>> ");
             String linea = reader.readLine();
-            if(linea == null) break; // Presionar Ctrl + D
+            if(linea == null) break;
             ejecutar(linea);
-            existenErrores = false; // Restablece el indicador de errores
+            existenErrores = false;
         }
     }
 
@@ -54,9 +44,6 @@ public class Interprete {
         try{
             Scanner scanner = new Scanner(source);
             List<Token> tokens = scanner.scan();
-
-            // Imprime cada token generado
-
             for(Token token : tokens){
                 System.out.println(token);
             }
@@ -64,26 +51,16 @@ public class Interprete {
         catch (Exception ex){
             ex.printStackTrace();
         }
-
     }
 
-    /*
-    El método error se puede usar desde las distintas clases
-    para reportar los errores:
-    Interprete.error(....);
-     */
     static void error(int linea, String mensaje){
         reportar(linea, "", mensaje);
     }
 
     private static void reportar(int linea, String posicion, String mensaje){
-
-        // Imprime mensajes de error formateados en la consola
-
         System.err.println(
                 "[linea " + linea + "] Error " + posicion + ": " + mensaje
         );
         existenErrores = true;
     }
-
 }
---
+++ src/parser/ExprAssign.java
@@ -0,0 +1,13 @@
+package parser;
+
+import scanner.Token;
+
+public class ExprAssign extends Expression{
+    final Token name;
+    final Expression value;
+
+    ExprAssign(Token name, Expression value) {
+        this.name = name;
+        this.value = value;
+    }
+}
